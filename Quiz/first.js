23498fdde9a0780ae77216a19b350ebd0838f63f
satya gurjar <satyagurjar405@gmail.com>
2025-12-31 11:04:57 +0530

    add Quiz

---
+++ Quiz/first.js
@@ -0,0 +1,26 @@
+const form  = document.querySelector('form');
+
+const answers = {
+    q1:"Sachin Tendulkar",
+    q2:"West Indies",
+    q3:"Sachin Tendulkar",
+    q4:'264',
+    q5:'Muttiah Muralitharan'
+
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const data = new FormData(form);
+    let finalScore = 0;
+
+
+    for(let [name,value] of data.entries()){
+        if(answers[name]==value)
+            finalScore++;
+    }
+
+
+    document.getElementById('out').textContent = `Your final score is ${finalScore} out of 5`;
+})
